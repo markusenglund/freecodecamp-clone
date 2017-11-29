0da1a5f137967c5bf5f73bb460a1c226fed79d03
yogaboll <markus.s.englund@gmail.com>
2017-11-29 20:21:34 +0700

    Make challenge description more complete

--- src/app/components/Challenge/DomChallenge.jsx
+++ src/app/components/Challenge/DomChallenge.jsx
@@ -6,13 +6,21 @@ import "./DomChallenge.scss";
 
 class DomChallenge extends Component {
   render() {
-    const { description } = this.props;
+    const { description, name, tests } = this.props.challenge;
     return (
       <div>
         <div className="dom-challenge-description">
-          {description.map(descriptionHtml => (
-            <div dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
-          ))}
+          <h1 className="dom-challenge-name">{name}</h1>
+          <div>
+            {description.map(descriptionHtml => (
+              <div dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
+            ))}
+          </div>
+          <button className="dom-challenge-button">
+            Run tests (Ctrl + Enter)
+          </button>
+          <h2>Test-stuff incomplete</h2>
+          <div>{tests.map(test => <div>{test}</div>)}</div>
         </div>
       </div>
     );
@@ -20,13 +28,16 @@ class DomChallenge extends Component {
 }
 
 DomChallenge.propTypes = {
-  description: PropTypes.arrayOf(PropTypes.string).isRequired
+  challenge: PropTypes.shape({
+    description: PropTypes.arrayOf(PropTypes.string),
+    name: PropTypes.string
+  }).isRequired
 };
 
 const mapStateToProps = state => {
   const currentChallengeName = state.router.pathname.split("/")[2];
-  const { description } = state.challenges[currentChallengeName];
-  return { description };
+  const challenge = state.challenges[currentChallengeName];
+  return { challenge };
 };
 
 export default connect(mapStateToProps)(DomChallenge);
