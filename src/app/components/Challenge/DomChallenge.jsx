dbd96715d376d78b29afc61fdd292e3932382f8f
yogaboll <markus.s.englund@gmail.com>
2017-12-02 21:28:25 +0700

    test assertions work now

--- src/app/components/Challenge/DomChallenge.jsx
+++ src/app/components/Challenge/DomChallenge.jsx
@@ -2,18 +2,44 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+import /* { assert } */ chai from "chai";
+import jquery from "jquery";
 import Editor from "./Editor";
 import Preview from "./Preview";
 import "./DomChallenge.scss";
 
 class DomChallenge extends Component {
-  constructor() {
-    super();
-    this.state = { code: "<!-- Write code here -->" };
+  constructor(props) {
+    super(props);
+    const { challenge } = this.props;
+    this.state = {
+      code: challenge.challengeSeed[0],
+      error: null,
+      testsPassed: false
+    };
   }
 
   updateCode = code => {
     this.setState({ code });
+  };
+
+  testCode = () => {
+    // const { code } = this.state;
+    const { tests } = this.props.challenge;
+    /* eslint-disable no-unused-vars */
+    const { assert } = chai;
+    const $ = jquery;
+    /* eslint-enable no-unused-vars */
+
+    try {
+      tests.forEach(test => {
+        // console.log(test.text, test.testString, code);
+        eval(test.testString);
+        this.setState({ testsPassed: true });
+      });
+    } catch (e) {
+      this.setState({ error: e });
+    }
   };
 
   render() {
@@ -28,10 +54,12 @@ class DomChallenge extends Component {
               dangerouslySetInnerHTML={{ __html: descriptionHtml }}
             />
           ))}
-          <button className="dom-challenge-button">
+          <button className="dom-challenge-button" onClick={this.testCode}>
             Run tests (Ctrl + Enter)
           </button>
           <h2>Test-stuff incomplete</h2>
+          {this.state.testsPassed && "The tests have motherfucking passed"}
+          <div dangerouslySetInnerHTML={{ __html: this.state.error }} />
           {/* <div>{tests.map(test => <div>{test}</div>)}</div> */}
         </div>
         <Editor code={this.state.code} updateCode={this.updateCode} />
@@ -44,7 +72,8 @@ class DomChallenge extends Component {
 DomChallenge.propTypes = {
   challenge: PropTypes.shape({
     description: PropTypes.arrayOf(PropTypes.string),
-    name: PropTypes.string
+    name: PropTypes.string,
+    tests: PropTypes.arrayOf(PropTypes.object)
     // tests: PropTypes.arrayOf(PropTypes.string)
   }).isRequired
 };
