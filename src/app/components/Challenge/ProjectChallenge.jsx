144576fefde9b40bad1aac117b5a376c105cd263
Markus <markus.s.englund@gmail.com>
2017-12-21 21:21:02 +0100

    improve project challenge

--- src/app/components/Challenge/ProjectChallenge.jsx
+++ src/app/components/Challenge/ProjectChallenge.jsx
@@ -9,17 +9,31 @@ import { connect } from "react-redux";
 
 class ProjectChallenge extends Component {
   render() {
-    const { description } = this.props.challenge;
+    const { description, challengeSeed: imageIds } = this.props.challenge;
 
     if (!description) {
       return <div>SPINNER</div>;
     }
 
     return (
-      <div className="dom-challenge-description">
-        {description.map((descriptionHtml, i) => (
-          <div key={i} dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
-        ))}
+      <div className="project-challenge">
+        <div className="dom-challenge-description">
+          {description.map((descriptionHtml, i) => (
+            <div
+              key={i}
+              dangerouslySetInnerHTML={{ __html: descriptionHtml }}
+            />
+          ))}
+        </div>
+        <div className="challenge-images">
+          {imageIds.map(imageId => (
+            <img
+              src={`https://imgur.com/${imageId}.png`}
+              alt="Example project"
+              key={imageId}
+            />
+          ))}
+        </div>
       </div>
     );
   }
@@ -28,7 +42,7 @@ class ProjectChallenge extends Component {
 ProjectChallenge.propTypes = {
   challenge: PropTypes.shape({
     description: PropTypes.array,
-    dashedName: PropTypes.string
+    challengeSeed: PropTypes.array
   }).isRequired
   // nextChallengeName: PropTypes.string.isRequired,
   // dispatch: PropTypes.func.isRequired
