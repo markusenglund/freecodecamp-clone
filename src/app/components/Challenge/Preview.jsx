21b2d686273db484bfd2193c67aa6a29c95bcd73
Markus <markus.s.englund@gmail.com>
2017-12-21 11:12:54 +0100

    make functioning phone shell for preview window

--- src/app/components/Challenge/Preview.jsx
+++ src/app/components/Challenge/Preview.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import iphoneFrame from "../../../assets/images/iphone-frame-cropped.png";
 
 class Preview extends Component {
   render() {
     const { code } = this.props;
     return (
-      <div className="challenge-preview">
-        <div dangerouslySetInnerHTML={{ __html: code }} />
+      <div className="challenge-preview-wrapper">
+        <div className="challenge-preview">
+          <img
+            className="challenge-preview-image"
+            src={iphoneFrame}
+            alt="iphone-frame"
+          />
+          <div
+            dangerouslySetInnerHTML={{ __html: code }}
+            className="challenge-preview-text"
+          />
+        </div>
       </div>
     );
   }
